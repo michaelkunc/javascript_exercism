6ef0234d500e48d18adb30eb8f814733225bf84d
michaelkunc <mjkunc@gmail.com>
2015-09-24 10:31:06 -0500

    object oriented refactor of hamming

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -1,17 +1,19 @@
-var hamming = {
-    compute: function(strand1, strand2) {
-        if (strand1.length != strand2.length)
-            throw "DNA strands must be of equal length.";
-
-        var difference = 0;
-
-        strand1.split('').forEach(function(element, index) {
-            if (element != strand2[index])
-                difference++;
-        });
-
-        return difference;
-    }
+module.exports = {
+    compute: hamming
 };
 
-module.exports = hamming;
+function hamming(strand1, strand2) {
+    if (strand1.length != strand2.length) {
+        throw "DNA strands must be of equal length.";
+        return;
+    }
+    return difference(strand1, strand2);
+}
+
+function difference(strand1, strand2) {
+    var count = 0;
+    strand1.split('').forEach(function(element, index) {
+        if (element != strand2[index]) count++;
+    });
+    return count;
+}
