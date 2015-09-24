efbe8abe64530cfc4db91eaeb7bd06e1e151b261
michaelkunc <mjkunc@gmail.com>
2015-09-24 09:17:20 -0500

    hamming refactor

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -1,18 +1,17 @@
-var Hamming = {
+var hamming = {
     compute: function(strand1, strand2) {
-        if (strand1.length != strand2.length) {
+        if (strand1.length != strand2.length)
             throw "DNA strands must be of equal length.";
-        } else {
-            var difference = 0;
-            for (var i = 0; i < strand1.length; i++) {
-                if (strand1[i] != strand2[i]) {
-                    difference = difference + 1;
-                }
-            }
-            return difference;
-        }
+
+        var difference = 0;
+
+        strand1.split('').forEach(function(element, index) {
+            if (element != strand2[index])
+                difference++;
+        });
+
+        return difference;
     }
 };
 
-module.exports = Hamming;
-
+module.exports = hamming;
