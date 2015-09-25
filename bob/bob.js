bbfe9714148a34aff210673ae5864036f87eaca8
michaelkunc <mjkunc@gmail.com>
2015-09-25 09:45:09 -0500

    refactored bob

--- bob/bob.js
+++ bob/bob.js
@@ -3,30 +3,37 @@
 // convenience to get you started writing code faster.
 //
 
+var Bob = function() {
 
-var Bob = function() {};
+  this.hey = function(input) {
+    if (isLoud(input)) return responses.chill;
 
-Bob.prototype.hey = function(input) {
-    if (input === input.toUpperCase() && /[a-z]/i.test(input)) {
-        return responses.chill;
-    }
+    if (isQuestion(input)) return responses.answer;
 
-    if (input.slice(-1) == '?') {
-        return responses.answer;
-    }
-
-    if (input.trim().length === 0){
-      return responses.silentTreatment;
-    }
+    if (isSilent(input)) return responses.silentTreatment;
 
     return responses.whatever;
-};
+  };
 
-var responses = {
-  chill: 'Whoa, chill out!',
-  answer: 'Sure.',
-  silentTreatment: 'Fine. Be that way!',
-  whatever: 'Whatever.'
+  var responses = {
+        chill: 'Whoa, chill out!',
+        answer: 'Sure.',
+        silentTreatment: 'Fine. Be that way!',
+        whatever: 'Whatever.'
+    },
+
+    isLoud = function(input) {
+        return input === input.toUpperCase() && /[a-z]/i.test(input);
+    },
+
+    isQuestion = function(input) {
+        return input.slice(-1) === '?';
+    },
+
+    isSilent = function(input) {
+        return input.trim().length === 0;
+    };
+
 };
 
 module.exports = Bob;
