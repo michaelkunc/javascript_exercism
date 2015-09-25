65e86218643e0835bb4fc736bdd6707face42333
michaelkunc <mjkunc@gmail.com>
2015-09-25 08:47:20 -0500

    refactored rna_transcription

--- rna-transcription/rna_transcription.js
+++ rna-transcription/rna_transcription.js
@@ -1,16 +1,14 @@
-var toRna = function(dna){
-  var dnaToRna = {
-    'G' : 'C',
-    'C' : 'G',
-    'T' : 'A',
-    'A' : 'U'
-  }
-  var rna = [];
-  var dna = dna.split('')
-  for (var i = 0; i < dna.length; i++){
-    rna.push(dnaToRna[dna[i]]);
-  }
-  return rna.join('');
-};
-
 module.exports = toRna;
+
+function toRna(dna){
+  return dna.split('').map(function(index){
+   return dnaToRna[index];
+  }).join('');
+}
+
+var dnaToRna = {
+    G: 'C',
+    C: 'G',
+    T: 'A',
+    A: 'U'
+};
--- rna-transcription/rna_transcription_test.spec.js
+++ rna-transcription/rna_transcription_test.spec.js
@@ -1,3 +1,4 @@
+
 var toRna = require('./rna_transcription');
 
 describe('toRna()', function() {
